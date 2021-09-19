cc71fae04512e3e6fe2aed2ce8a15d5c52dead29
Sergio Rodriguez <ingensiscomescom@outlook.com>
2021-09-18 19:39:55 -0500

    Se agrego el script para actualización en Ubuntu y derivados

---
+++ actualizacion.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo apt-get update
+
+echo 
+echo "------------> apt-get update termino <------------"
+
+sudo apt-get upgrade
+
+echo
+echo "------------> apt-get update termino <------------"
+
+exit
